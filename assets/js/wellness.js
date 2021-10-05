267aac55456a00020a1d47be4bfe4e87b72ac67d
Robert Anthony Williams <rawc@me.com>
2021-10-04 21:30:45 -0600

    Completed js code to include console.log, and corrected errors in html code (lines 126 and 134).

--- assets/js/wellness.js
+++ assets/js/wellness.js
@@ -5,7 +5,11 @@ var searchExercisePlan = document.querySelector('#exercise');
 var searchFirstNameTextBox = document.querySelector('#fname');
 var searchLastNameTextBox = document.querySelector('#lname');
 var searchGenderDropDown = document.querySelector('#gender');
-
+var searchAgeTextBox = document.querySelector('#age');
+var searchHeightTextBox = document.querySelector('#height');
+var searchWeightTextBox = document.querySelector('#weight');
+var searchfoodPreferenceDropDown = document.querySelector('#foodPreference');
+var searchobjectDropDown = document.querySelector('#object');
 
 function bmiHeatMap() {
     searchBmiForm.classList.add('hide');
@@ -16,6 +20,16 @@ function bmiHeatMap() {
     console.log(lastName);
     var gender = searchGenderDropDown.value;
     console.log(gender);
+    var age = searchAgeTextBox.value;
+    console.log(age);
+    var height = searchHeightTextBox.value;
+    console.log(height);
+    var weight = searchWeightTextBox.value;
+    console.log(weight);
+    var foodPreference = searchfoodPreferenceDropDown.value;
+    console.log(foodPreference);
+    var object = searchobjectDropDown.value;
+    console.log(object);
 }
 
 function nutritionPlan() {
