30db8bda590f535ba736d61f25971b0e94299a09
Robert Anthony Williams <rawc@me.com>
2021-10-10 12:22:12 -0600

    Hiding the images of the nutrition dropdown.

--- assets/js/nutrition.js
+++ assets/js/nutrition.js
@@ -6,9 +6,9 @@ var buttonEl = document.querySelector(".nutrButton");
 var nutrValueLabel = document.querySelector('#nutrValue');
 
 
-async function getNutrition (nutritionEl) {
+async function getNutrition(nutritionEl) {
     var url = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?q=' + nutritionEl;
-    const response = await fetch (url, {
+    const response = await fetch(url, {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
@@ -25,7 +25,7 @@ async function getNutrition (nutritionEl) {
     nutrValueLabel.innerHTML = '';
     //nutrResults.innerHTML = nutrResponse.getNutrition;
 
-    for (let i=0; i < nutrCategory.length; i++) {
+    for (let i = 0; i < nutrCategory.length; i++) {
         let singleItem = nutrCategory[i];
 
         let value = singleItem.value.replace(/\\/g, '');
@@ -35,7 +35,7 @@ async function getNutrition (nutritionEl) {
 
         //add a fetch and get response, convert response data using response.json(), store response.json() inside singleRecipe, console.log(singleRecipe.recipes[0].url)
         nutrValueLabel.innerHTML += `<p>${parseObject.title}</p><img src="https://spoonacular.com/recipeImages/${parseObject.id}-240x150.jpg" className="recipe-image" />`
-        //nutrValueLabel.innerHTML += singleItem.value.id;
+            //nutrValueLabel.innerHTML += singleItem.value.id;
 
         // this new fetch needs to go after the response.json() single recipe, make sure to replace the id in the new fetch request from 2nd % onwards with this "${parseObject.id}-240150.jpg"
     }
@@ -43,15 +43,13 @@ async function getNutrition (nutritionEl) {
 
 buttonEl.addEventListener("click", getNutrition);
 
-
-
 var nutritionSection = document.querySelector('.hide');
 var searchBmiOutput = document.querySelector('#BMI-suggestions');
 var searchOutput = document.querySelector('#back');
 var output = document.querySelector('#nutritionEl');
 var button = document.querySelector('.nutrButton');
 var plan = document.querySelector('.nutrPlan');
-
+var container = document.querySelector('#responseContainer');
 
 
 function backButton() {
@@ -61,6 +59,7 @@ function backButton() {
     output.classList.add('hide');
     button.classList.add('hide');
     plan.classList.add('hide');
+    container.classList.add('hide');
 }
 
 
@@ -78,7 +77,7 @@ function backButton() {
 // var back=document.getElementById('back');
 
 // function backButton(){
-    
+
 //     if(text.classList==='newClass'){
 //         text.classList.remove('newClass')
 //     }
